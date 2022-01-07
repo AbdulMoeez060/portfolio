fc859f32a6439d31e3ede716253b980f0111936c
AbdulMoeez060 <76743473+AbdulMoeez060@users.noreply.github.com>
2022-01-07 15:47:52 +0500

    Added content to intro and made an animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,24 @@
+import "./intro.css"
 function Intro() {
     return (
-        <div>
-            this is intro!
+        <div className="intro">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My Name is</h2>
+                    <h1 className="i-name">Abdul Moeez</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">React Dev</div>
+                            <div className="i-title-item">AI/ML Enthusiast</div>
+                            <div className="i-title-item">Always Tired</div>
+                            <div className="i-title-item">Binge Reader</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">right</div>
         </div>
     )
 }
